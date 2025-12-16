5ea5f865898efe62c7e3ecb15b7c4f9d10278396
luca <dalessandroluca1@gmail.com>
2025-12-16 18:57:17 +0100

    deleted useless files

--- EspOTA/OtaDebug.h
+++
@@ -1,9 +0,0 @@
-#ifndef OTA_DEBUG_H
-#define OTA_DEBUG_H
-
-#include <CommonDebug.h>
-
-#define OTA_LOG(msg) DBG_LOG("*OTA:", msg)
-#define OTA_LOGF(fmt, ...) DBG_LOGF("*OTA:", fmt, ##__VA_ARGS__)
-
-#endif
